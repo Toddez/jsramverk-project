025c403a25b622e2bb7cd601fe3c7d2aac2ce6c4
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-20 15:27:31 +0200

    Added index to render app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx'; // eslint-disable-line no-unused-vars
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
