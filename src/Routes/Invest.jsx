677b0972fb78fe18e450d98904376f0a6ff5d8bd
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-21 16:07:12 +0200

    Get list of stocks and display them

--- src/Routes/Invest.jsx
+++ src/Routes/Invest.jsx
@@ -2,19 +2,67 @@ import React from 'react';
 
 import { withRouter, Link } from 'react-router-dom'; // eslint-disable-line no-unused-vars
 
+import { api_url } from '../Models/config';
+import auth from '../Models/auth';
+
 class Invest extends React.Component {
+    _isMounted = false;
+
     constructor(props) {
         super(props);
 
         this.state = {
-
+            stocks: []
         };
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+
+        fetch(`${api_url}/stocks`, {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.errors) {
+                    return;
+                }
+
+                auth.data.stocks = res.stocks;
+
+                if (this._isMounted) {
+                    this.setState({
+                        stocks: auth.data.stocks
+                    });
+                }
+            });
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
     }
 
     render() {
         return (
-            <div>
-                <Link to="/about/1">Fond/Aktie 1</Link>
+            <div className='stock-list'>
+                <h3>Aktier</h3>
+                <div className='stock-item'>
+                    <div>Namn</div>
+                    <div className='stock-value'>Pris/andel</div>
+                </div>
+                {
+                    this.state.stocks.map((value, index) => {
+                        return (
+                            <div className='stock-item' key={index}>
+                                <Link to={`/about/${value.id}`}>{value.name}</Link>
+                                <div className='stock-value'>{value.current} kr</div>
+                            </div>
+                        );
+                    })
+                }
             </div>
         );
     }
