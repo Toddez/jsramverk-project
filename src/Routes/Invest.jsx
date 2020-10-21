d2aa373c517fda5a45a05974657e1031038750a5
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-21 15:09:30 +0200

    Made invest use router

--- src/Routes/Invest.jsx
+++ src/Routes/Invest.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
+import { withRouter, Link } from 'react-router-dom'; // eslint-disable-line no-unused-vars
+
 class Invest extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {};
+        this.state = {
+
+        };
     }
 
     render() {
         return (
-            <div>Unimplemented Component</div>
+            <div>
+                <Link to="/about/1">Fond/Aktie 1</Link>
+            </div>
         );
     }
 }
 
-export default Invest;
+export default withRouter(Invest);
