edae14db230fb839da1ebe1b2a5a9123d98ae5e3
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-23 16:15:37 +0200

    Tripple equals

--- src/Routes/Inventory.jsx
+++ src/Routes/Inventory.jsx
@@ -95,7 +95,7 @@ class Inventory extends React.Component {
                     </div>
                     <div className='inv-list'>
                         {this.state.inventory.map((value, index) => {
-                            if (value.amount == 0) {
+                            if (value.amount === 0) {
                                 return null;
                             }
 
