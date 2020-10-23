08d3b178c8955d08473ee06bcab359ac5fe53ec9
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-23 15:43:51 +0200

    Localization: eng to swe

--- src/Routes/Login.jsx
+++ src/Routes/Login.jsx
@@ -73,7 +73,7 @@ class Login extends React.Component {
                         </label>
 
                         <label htmlFor="password">
-                            Password
+                            Lösenord
                             <input type="password" name="password" id="password" placeholder="******" required value={this.state.password} onChange={this.handleChange} />
                         </label>
 
