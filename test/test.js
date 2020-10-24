c592141d83a01b820bd8426ac82e5f6fc0b749ca
Teo Carlsson <carlsson.teo@gmail.com>
2020-10-24 15:45:32 +0200

    Added frontend tests using selenium webdriver

--- test/test.js
+++ test/test.js
@@ -2,9 +2,11 @@ const spawn = require('child_process').spawn;
 const assert = require('assert');
 const test = require('selenium-webdriver/testing');
 const webdriver = require('selenium-webdriver');
+const { exit } = require('process');
 const By = webdriver.By;
+const until = webdriver.until;
 
-let server;
+spawn('node', ['node_modules/http-server/bin/http-server', '-s', '-p', '3000', 'build', '&']);
 
 const chromeOptions = {
     'args': ['--no-sandbox', '--headless']
@@ -17,18 +19,44 @@ const browser = new webdriver.Builder()
     .withCapabilities(chromeCapabilities)
     .build();
 
-test.describe('jsramverk me', () => {
+test.describe('project', () => {
+    const go = (target, callback) => {
+        browser.findElement(By.linkText(target)).then((element) => {
+            element.click();
+            callback();
+        });
+    };
+
     const urlMatches = (target) => {
         browser.getCurrentUrl().then((url) => {
             assert.ok(url.endsWith(target));
         });
     };
 
+    const hasH2 = async (targets) => {
+        if (!Array.isArray(targets))
+            targets = [targets];
+
+        let promises = [];
+        targets.forEach(async (target) => {
+            promises.push(new Promise((resolve) => {
+                browser.wait(until.elementsLocated(By.xpath(`//h2[contains(text(), '${target}')]`)), 5000)
+                    .then((result) => {
+                        assert.ok(result);
+                        resolve();
+                    })
+                    .catch(() => {
+                        assert.fail(`h2: ${target} not found`);
+                        resolve();
+                    });
+            }));
+        });
+
+        return Promise.all(promises);
+    };
+
     test.beforeEach(function (done) {
         this.timeout(20000);
-
-        console.log('SPAWNING SERVER');
-        server = spawn('node_modules/http-server/bin/http-server', ['-s', '-p 3000', 'build']);
 
         browser.get('http://localhost:3000');
         done();
@@ -36,17 +64,51 @@ test.describe('jsramverk me', () => {
 
     test.after(function (done) {
         browser.quit();
-
-        console.log('KILLING SERVER');
-        server.kill();
         done();
+        exit(0);
     });
 
     test.it('should load index', (done) => {
-        console.log('SHOULD GET INDEX TEST');
         urlMatches('/');
 
         done();
+    });
+
+    test.it('should go to register through login form', (done) => {
+        go('Logga in', () => {
+            go('Registrera här', () => {
+                urlMatches('/register');
+                done();
+            });
+        });
+    });
+
+    test.it('should go list of stocks', (done) => {
+        go('Investera', () => {
+            urlMatches('/invest');
+            done();
+        });
+    });
+
+    test.it('should go specific stock', (done) => {
+        go('Investera', () => {
+            go('AMD', () => {
+                hasH2('AMD');
+                done();
+            });
+        });
+    });
+
+    test.it('should go to index from anywhere', (done) => {
+        go('Investera', () => {
+            go('AMD', () => {
+                hasH2('AMD');
+                go('PROJEKT', () => {
+                    urlMatches('/');
+                    done();
+                });
+            });
+        });
     });
 });
 
